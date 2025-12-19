d064fc41a83355b72af1a33b95d0f3fbb6fd9685
Evan Alford <evan.al4d@gmail.com>
2025-12-19 11:15:14 -0600

    added fetch for colony inventory, govern, minerals

---
+++ api/fetchCalls.js
@@ -0,0 +1,14 @@
+//fetch governors
+export const getGovernors = async () => {
+    return fetch(`http://localhost:8088/Governor?_expand=colony`).then((res) => 
+    res.json())
+}
+//fetch colony inventory
+export const getColonyInventory = async () => {
+    return fetch(`http://localhost:8088/colonyMinerals?_expand=colony&_expand=mineral`).then((res) => res.json())
+}
+
+//Fetch minerals
+export const getMinerals = async () => {
+    return fetch (`http://localhost:8088/minerals`).then((res) => res.json())
+}
